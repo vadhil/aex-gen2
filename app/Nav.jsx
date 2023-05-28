d58aedcc109e35588358ee8f7ffda5beb430358d
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-28 16:26:51 +0800

    nav smooth and menu

--- app/Nav.jsx
+++ app/Nav.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 
 export default function Nav() {
   const [color, setColor] = useState(false)
+  const [click, setClick] = useState(false)
     const changeColor = () => {
         // 680
         if (window.scrollY >= 580 ) {
@@ -33,13 +34,13 @@ export default function Nav() {
      <div className="flex justify-between  ">
 
     <div className="logo text-">Logo</div>
-    <nav className="ms-aut  hidden gap-4 md:flex">
+    <nav className={`ms-aut  ${click && "hidden"} gap-4 md:flex`}>
      <Link href={'/'}>translate</Link>
      <Link href={'/'}>pesan website</Link>
      <Link href={'/'}>tentang kami</Link>
     </nav>
     <div className="px-4">
-     <button type="checkbox ">Menu</button>
+     <button type="checkbox" onClick={()=> setClick(!click)}>Menu</button>
     </div>
      </div>
  </header>
