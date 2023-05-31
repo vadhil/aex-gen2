195a2d4d9cc61d8330ee4cea4c8050079ebeb801
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-31 14:26:27 +0800

    consult

--- app/Pink.jsx
+++ app/Pink.jsx
@@ -1,14 +1,28 @@
 "use client"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 import { useState } from "react"
-import { Button, ButtonPrimary } from "./components/Button"
+import { db } from "./config/firebase-config"
+import { redirect } from "next/navigation"
 
 export default function Pink() {
    const [name, setName] = useState('')
    const [email, setEmail] = useState('')
+   const [hp, setHp] = useState('')
+
+
+   const CreateHandler = async() => {
+      await addDoc(collection(db, "consult"), {
+        time: serverTimestamp(),
+        name,
+        email,
+        hp
+      })
+      redirect("/")
+   }
 
 
 return (
-<section className="bg-secondary py-16 ">
+<section className="bg-secondary py-16 " id="pink">
   <div className="bg-secondary shadow-sm">
 
     <h2 className="sub-title-primary">KONSULTASI GRATIS UNTUK MENCIPTAKAN <br /> 
@@ -16,7 +30,7 @@ return (
     <p className=" text-center text-gray-500 py-5">Beritahukan kepada kami 
     goals dan website impian anda</p>
     <div className="pink-container flex flex-col  gap-9 justify-center items-center">
-        <form action="" className="flex flex-col gap-3 py-">
+        <form onSubmit={CreateHandler} className="flex flex-col gap-3 py-">
             <label className="block" >
                 <input required onChange={(e)=> setName(e.target.value)} value={name}
                   className=" input-primary"
@@ -32,7 +46,7 @@ return (
                 />
               </label>
             <label className="block w-96">
-                <input required onChange={(e)=> setEmail(e.target.value)} value={email}
+                <input required onChange={(e)=> setHp(e.target.value)} value={hp}
                   type="text"
                   className="input-primary
                   "
@@ -40,7 +54,7 @@ return (
                 />
               </label>
               <div className="flex justify-center">
-                <button className="button-primary mt-5">Kirim</button>
+                <button type="submit" className="button-primary mt-5">Kirim</button>
               </div>
         </form>
     </div>
