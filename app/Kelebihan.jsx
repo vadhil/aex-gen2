27c63214717e1426529a3322fe171b05f83bb9c9
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-31 22:47:36 +0800

    idk

--- app/Kelebihan.jsx
+++ app/Kelebihan.jsx
@@ -1,19 +1,18 @@
 import Image from "next/image"
 
 export default function Kelebihan() {
-    const Card = (src, title, content) => {
+    const Card = (animate, src, title, content) => {
         return (
           <div className="CARD group border-primary border- 
           border-spacing-5 p-4/ rounded-lg shadow-md hover:shadow-xl z-10
           relative duration-150 transition-shadow ease-in
           overflow-hidden 
            h-[18rem] w-[18rem] 64 bg-white">
-          <div className="img-card rounded-full py-5 flex">
+          <div className={`img-card rounded-full py-5 flex group-hover:animate-${animate}`}>
           <Image width={130} height={130} src={src} 
-          className="mx-auto p-5 group-hover:animate-bounce
-           transition-all hover:animate-pulse duration-300 ease-in delay-100 
-           
-          " alt="" />
+          className={`mx-auto p-5 group-hover:animate-bounce/
+           transition-all animate-/ duration-300 animate-bounce/
+            ease-in delay-100`} alt="" />
             </div>
           <div className="text-card p-4 flex flex-col absolute 
           justify-center items-center/ top-[7rem] text-center  ">
@@ -35,11 +34,11 @@ export default function Kelebihan() {
                 Kami tidak hanya fokus pada <span className="text-primary">jasa pembuatan website</span>, tetapi juga memberikan layanan modifikasi desain website yang dapat meningkatkan kinerja bisnis Anda di Indonesia</h3>
         </div>
             <div className="card-box flex flex-wrap justify-center lg:mx-[10rem]  gap-3">
-                {Card("/3drocket.png", "Anti Lemot", "Bebas dari keterlambatan, jasa pembuatan website kami menawarkan kecepatan tanpa hambatan.")}
-                {Card("/3dwww.png", "SEO Friendly", "Mengoptimalkan peringkat pencarian, kami menciptakan website yang ramah SEO untuk keberhasilan online Anda.")}
-                {Card("/responsive.png", "Responsive", "Tampil sempurna di setiap perangkat, desain responsif kami menjawab kebutuhan pengguna mobile.")}
-                {Card("/unique-design.png", "Design Premium", "Kesempurnaan visual, website dengan desain premium yang memukau akan meningkatkan citra merek Anda.")}
-                {Card("/maintenance.png", "Gratis Maintenance", "Lebih dari sekadar pembuatan, kami juga menyediakan layanan perawatan website yang andal.")}
+                {Card("bounce","/3drocket.png", "Anti Lemot", "Bebas dari keterlambatan, jasa pembuatan website kami menawarkan kecepatan tanpa hambatan.")}
+                {Card("pulse","/3dwww.png", "SEO Friendly", "Mengoptimalkan peringkat pencarian, kami menciptakan website yang ramah SEO untuk keberhasilan online Anda.")}
+                {Card("bounce","/responsive.png", "Responsive", "Tampil sempurna di setiap perangkat, desain responsif kami menjawab kebutuhan pengguna mobile.")}
+                {Card("pulse","/unique-design.png", "Design Premium", "Kesempurnaan visual, website dengan desain premium yang memukau akan meningkatkan citra merek Anda.")}
+                {Card("spin","/maintenance.png", "Gratis Maintenance", "Lebih dari sekadar pembuatan, kami juga menyediakan layanan perawatan website yang andal.")}
             </div>
     </div> 
     </section>
